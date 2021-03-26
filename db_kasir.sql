3a18c907870b6ad1bcfe5f98445151aa5074572c
muhammad rizqi akbar <30398861+rizqiakbar16@users.noreply.github.com>
2021-03-26 10:13:43 +0700

    Add files via upload

---
+++ db_kasir.sql
@@ -0,0 +1,286 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2021 at 04:12 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kasir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` varchar(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `id_kategori` varchar(10) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `id_satuan` varchar(10) NOT NULL,
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` varchar(20) NOT NULL,
+  `id_barang` varchar(10) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `jumlah_barang` int(3) NOT NULL,
+  `total_harga` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `detail_transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `trigger_total_harga` BEFORE INSERT ON `detail_transaksi` FOR EACH ROW SET NEW.total_harga = NEW.jumlah_barang * NEW.harga
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori_barang`
+--
+
+CREATE TABLE `kategori_barang` (
+  `id_kategori` varchar(10) NOT NULL,
+  `kategori_barang` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satuan_barang`
+--
+
+CREATE TABLE `satuan_barang` (
+  `id_satuan` varchar(10) NOT NULL,
+  `nama_satuan` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_keluar`
+--
+
+CREATE TABLE `stok_keluar` (
+  `id_stok_keluar` varchar(10) NOT NULL,
+  `id_barang` varchar(10) NOT NULL,
+  `tgl_keluar` date NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `keterangan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok_masuk`
+--
+
+CREATE TABLE `stok_masuk` (
+  `id_stok_masuk` varchar(10) NOT NULL,
+  `id_barang` varchar(10) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `jumlah_barang` int(10) NOT NULL,
+  `id_supplier` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id_supplier` varchar(10) NOT NULL,
+  `nama_supplier` varchar(30) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat_perusahaan` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` varchar(20) NOT NULL,
+  `tgl_transaksi` date NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `total_bayar` int(10) NOT NULL,
+  `total_kembalian` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `trg_total_harga` BEFORE INSERT ON `transaksi` FOR EACH ROW begin
+       DECLARE id_exists Boolean;
+       -- Check BookingRequest table
+       SELECT 1
+       INTO @id_exists
+       FROM penjualan
+       WHERE penjualan.id_transaksi= NEW.id_transaksi;
+
+       IF @id_exists = 1
+       THEN
+           UPDATE transaksi
+           SET total_harga = penjualan.total_harga
+           WHERE id_transaksi = NEW.id_transaksi;
+        END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` int(1) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `no_telp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `fk_id_kategori` (`id_kategori`),
+  ADD KEY `fk_id_satuan` (`id_satuan`),
+  ADD KEY `fk_username_barang` (`username`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `fk_id_transaksi` (`id_transaksi`),
+  ADD KEY `fk_id_barang` (`id_barang`);
+
+--
+-- Indexes for table `kategori_barang`
+--
+ALTER TABLE `kategori_barang`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `satuan_barang`
+--
+ALTER TABLE `satuan_barang`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `stok_keluar`
+--
+ALTER TABLE `stok_keluar`
+  ADD PRIMARY KEY (`id_stok_keluar`),
+  ADD KEY `fk_id_barang_keluar` (`id_barang`);
+
+--
+-- Indexes for table `stok_masuk`
+--
+ALTER TABLE `stok_masuk`
+  ADD PRIMARY KEY (`id_stok_masuk`),
+  ADD KEY `fk_id_barang_masuk` (`id_barang`),
+  ADD KEY `fk_id_supplier_masuk` (`id_supplier`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `fk_username_transaksi` (`username`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `fk_id_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_barang` (`id_kategori`),
+  ADD CONSTRAINT `fk_id_satuan` FOREIGN KEY (`id_satuan`) REFERENCES `satuan_barang` (`id_satuan`),
+  ADD CONSTRAINT `fk_username_barang` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `fk_id_barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `fk_id_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+
+--
+-- Constraints for table `stok_keluar`
+--
+ALTER TABLE `stok_keluar`
+  ADD CONSTRAINT `fk_id_barang_keluar` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
+-- Constraints for table `stok_masuk`
+--
+ALTER TABLE `stok_masuk`
+  ADD CONSTRAINT `fk_id_barang_masuk` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `fk_id_supplier_masuk` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id_supplier`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_username_transaksi` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
